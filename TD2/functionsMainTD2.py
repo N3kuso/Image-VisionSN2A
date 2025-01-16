1bc58bcaaebd3ffbb0419d70ed736efa927a30f9
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-01-16 12:48:25 +0100

    add ImgLogarithme function

--- TD2/functionsMainTD2.py
+++ TD2/functionsMainTD2.py
@@ -10,3 +10,7 @@ import numpy as np
 def ImgNegative(img):
     # On inverse l'image -> Valeur max - Intensité lumineuse du pixel
     return np.max(img) - img
+
+def ImgLogarithme(img, c):
+    img.astype(np.double)
+    return (c * np.log10(img)).astype(np.uint8)
