2c1d0c179c4b8211ec652dec731175f67754582e
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-01-16 11:25:58 +0100

    add ImgNegative function

--- TD2/functionsMainTD2.py
+++ TD2/functionsMainTD2.py
@@ -5,3 +5,8 @@ Fonctions pour le script MainTD2.py
 @author: kooky
 """
 
+import numpy as np
+
+def ImgNegative(img):
+    # On inverse l'image -> Valeur max - Intensité lumineuse du pixel
+    return np.max(img) - img
