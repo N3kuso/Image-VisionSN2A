7e887fc6829e0bb629254b182f8d36959f7b8467
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-01-30 10:55:02 +0100

    add Q3

--- TD3/mainTD3.py
+++ TD3/mainTD3.py
@@ -8,6 +8,7 @@ Script pour le TD3 Image&Vision
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import skimage as ski
 
 ###########################################################
 # Q2 :
@@ -20,4 +21,16 @@ img_ballon = cv2.cvtColor(img_ballon, cv2.COLOR_BGR2RGB) # Conversion de l'image
 plt.figure(2)
 plt.imshow(img_ballon)
 plt.title("Ballon.jpg")
+plt.show()
+
+###########################################################
+# Q3 :
+###########################################################
+saltPepper_rate = 0.15
+img_ballon_noised = ski.util.random_noise(img_ballon, mode='s&p', amount=saltPepper_rate )
+
+# Affichage
+plt.figure(3)
+plt.imshow(img_ballon_noised)
+plt.title("Ballon.jpg Salt & Pepper")
 plt.show()
