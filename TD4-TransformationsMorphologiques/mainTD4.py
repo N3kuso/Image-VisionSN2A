3e13da80bb616f58f83a5e71833d2542cbdde7db
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-02-10 09:36:38 +0100

    add Q3

--- TD4-TransformationsMorphologiques/mainTD4.py
+++ TD4-TransformationsMorphologiques/mainTD4.py
@@ -37,5 +37,31 @@ axes[1].set_title("Image Niv. de Gris")
 axes[2].imshow(img_binarized, cmap='gray')
 axes[2].set_title("Image binarisé")
 
+###########################################################
+# Q2 :
+###########################################################
+kernel_size = (3,3) # Taille du noyau 
+kernel =  cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size) # Création d'un noyau carré avec OpenCv
+
+img_dilated = cv2.dilate(img_binarized, kernel) # Dilatation de l'image
+img_eroded = cv2.erode(img_binarized, kernel) # Erosion de l'image
+
+### Affichage ###
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10,15))
+fig = plt.figure(2)
+fig.suptitle("Dilatation/Erosion")
+
+# Image Binaire
+axes[0].imshow(img_binarized, cmap="gray")
+axes[0].set_title("Image binaire")
+
+# Image dilatée
+axes[1].imshow(img_dilated, cmap='gray')
+axes[1].set_title("Image dilatée")
+
+# Image erodée
+axes[2].imshow(img_eroded, cmap='gray')
+axes[2].set_title("Image erodée")
+
 plt.tight_layout()
 plt.show()
